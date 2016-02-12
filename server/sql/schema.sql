6077f9f28ac5efafd1210e31ceda675035e35c9d
Albert <kovalevskij.albert@gmail.com>
2016-02-12 09:33:52 +0200

    isAdmin

--- server/sql/schema.sql
+++ server/sql/schema.sql
@@ -5,8 +5,9 @@ meta.defineType 'aa.provider:CODE';
 meta.defineType 'aa.profileId:STRING';
 meta.defineType 'aa.name';
 meta.defineType 'aa.profileData';
+meta.defineType 'aa.isAdmin;;BOOLEAN';
 
-meta.defineEntity 'aa.ProviderAccount', 'provider;profileId;profileData;name';
+meta.defineEntity 'aa.ProviderAccount', 'provider;profileId;profileData;name;isAdmin';
 meta.defineEntity 'aa.Account', 'name';
 
 meta.createTable 'aa.Account',0,1;
