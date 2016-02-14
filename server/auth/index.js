de156f568024aa46d3dc593e8286609deb3511fa
Alexander Levin <alevin.ru@gmail.com>
2016-02-14 20:39:21 +0200

    rm local auth

--- server/auth/index.js
+++ server/auth/index.js
@@ -6,12 +6,10 @@ import config from '../config/environment';
 import Account from '../api/providerAccount/providerAccount.model';
 
 // Passport Configuration
-require('./local/passport').setup(Account, config);
 require('./facebook/passport').setup(Account, config);
 
 var router = express.Router();
 
-router.use('/local', require('./local'));
 router.use('/facebook', require('./facebook'));
 
 export default router;
