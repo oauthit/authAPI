5ea2c44ef2e209cfb13c1aec12dca56c114773f0
Albert <kovalevskij.albert@gmail.com>
2016-02-15 16:24:10 +0200

    operation fields as a value service

--- client/app/app.constant.js
+++ client/app/app.constant.js
@@ -1,13 +1,71 @@
-(function(angular, undefined) {
+(function (angular, undefined) {
   angular.module("authApiApp.constants", [])
 
-.constant("appConfig", {
-	"userRoles": [
-		"guest",
-		"user",
-		"admin"
-	]
-})
+    .constant("appConfig", {
+      "userRoles": [
+        "guest",
+        "user",
+        "admin"
+      ]
+    })
 
-;
+    .value('operationFields', [
+        {
+          key: 'owner',
+          type: 'input',
+          templateOptions: {
+            label: 'Owner',
+            type: 'text',
+            placeholder: 'Owner',
+            required: true,
+            disabled: true,
+            description: 'Owner of the operation'
+          }
+        },
+        {
+          key: 'contact',
+          type: 'input',
+          templateOptions: {
+            label: 'Contact',
+            type: 'text',
+            placeholder: 'Contact',
+            required: true,
+            disabled: true,
+            description: 'Contact which interacts in operation'
+          }
+        },
+        {
+          key: 'total',
+          type: 'input',
+          templateOptions: {
+            label: 'Total',
+            type: 'number',
+            placeholder: 'Total',
+            required: true,
+            description: 'Enter total'
+          }
+        },
+        {
+          key: 'currency',
+          type: 'select',
+          templateOptions: {
+            label: 'Select currency',
+            labelProp: 'sign',
+            description: 'Currency of the operation',
+            options: [
+              {
+                sign: '$',
+                name: 'Dollar'
+              },
+              {
+                sign: '€',
+                name: 'Euro'
+              }
+            ]
+          }
+        }
+      ]
+    )
+
+  ;
 })(angular);
--- client/app/domain/operation/add/operationAdd.controller.js
+++ client/app/domain/operation/add/operationAdd.controller.js
@@ -4,65 +4,9 @@
 
   class OperationAddController {
 
-    constructor() {
-
+    constructor(operationFields) {
       this.operation = {};
-      this.operationFields = [
-        {
-          key: 'owner',
-          type: 'input',
-          templateOptions: {
-            label: 'Owner',
-            type: 'text',
-            placeholder: 'Owner',
-            required: true,
-            disabled: true,
-            description: 'Owner of the operation'
-          }
-        },
-        {
-          key: 'contact',
-          type: 'input',
-          templateOptions: {
-            label: 'Contact',
-            type: 'text',
-            placeholder: 'Contact',
-            required: true,
-            disabled: true,
-            description: 'Contact which interacts in operation'
-          }
-        },
-        {
-          key: 'total',
-          type: 'input',
-          templateOptions: {
-            label: 'Total',
-            type: 'number',
-            placeholder: 'Total',
-            required: true,
-            description: 'Enter total'
-          }
-        },
-        {
-          key: 'currency',
-          type: 'select',
-          templateOptions: {
-            label: 'Select currency',
-            labelProp: 'sign',
-            description: 'Currency of the operation',
-            options: [
-              {
-                sign: '$',
-                name: 'Dollar'
-              },
-              {
-                sign: '€',
-                name: 'Euro'
-              }
-            ]
-          }
-        }
-      ];
+      this.operationFields = operationFields;
     }
 
     onSubmit() {
