883989060baf6b72d799d2d9c20a5b3fa43a3b1b
Albert <kovalevskij.albert@gmail.com>
2016-02-15 11:38:02 +0200

    abstract models

--- server/api/providerAccount/index.js
+++ server/api/providerAccount/index.js
@@ -7,7 +7,7 @@ var router = express.Router();
 import {hasRole} from '../../auth/auth.service';
 
 router.get('/', hasRole('admin'), controller.index);
-router.get('/me', controller.show);
+router.get('/me', controller.showMe);
 router.get('/:id', hasRole('admin'), controller.show);
 
 module.exports = router;
