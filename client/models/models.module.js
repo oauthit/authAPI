73d2800db5c38eaf7dcf88474529c818813331df
Albert <kovalevskij.albert@gmail.com>
2016-02-16 14:37:58 +0200

    before create

--- client/models/models.module.js
+++ client/models/models.module.js
@@ -6,7 +6,13 @@
       'js-data'
     ])
     .config(function (DSProvider, DSHttpAdapterProvider) {
-      angular.extend(DSProvider.defaults, {});
+      angular.extend(DSProvider.defaults, {
+        beforeCreate: function (resource, data, cb) {
+          console.log('fine', data);
+          data.id = uuid.v4();
+          cb(null, data);
+        }
+      });
       angular.extend(DSHttpAdapterProvider.defaults, {
         basePath: 'http://localhost:9000/api/opr/'
       });
