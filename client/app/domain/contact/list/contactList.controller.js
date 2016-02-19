14e65d237f56bfd783ecd7c09125e502776a9af6
Albert <kovalevskij.albert@gmail.com>
2016-02-19 17:21:41 +0200

    contactList

--- client/app/domain/contact/list/contactList.controller.js
+++ client/app/domain/contact/list/contactList.controller.js
@@ -3,14 +3,36 @@
 (function () {
 
   angular.module('authApiApp')
-    .controller('ContactListCtrl', function ($scope, Agent, CounterAgent, SettingsService, InitCtrlService) {
+    .controller('ContactListCtrl', function ($scope,
+                                             $state,
+                                             Agent,
+                                             CounterAgent,
+                                             Invite,
+                                             SettingsService,
+                                             ErrorsService,
+                                             InitCtrlService) {
 
       var vm = this;
 
       CounterAgent.findAll();
 
       angular.extend(InitCtrlService.init(vm), {
-        contacts: []
+        contacts: [],
+        createInvite: function () {
+          //TODO change with spinner
+          vm.spinner = 'spinner started';
+          Invite.create({
+            ownerId: SettingsService.getCurrentAgent().id
+          }).then(function (response) {
+            vm.spinner = 'spinner stopped';
+            $state.go('debt.invite.info', {id: response.id});
+          }, function (err) {
+            ErrorsService.addError(err);
+          });
+        },
+        disableCreateInvite: function () {
+          return vm.spinner === 'spinner started';
+        }
       });
 
       function setAgent(agent) {
