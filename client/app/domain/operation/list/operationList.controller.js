0c8e337f6be0223573f42b8e8491755c064dde0f
Alexander Levin <alevin.ru@gmail.com>
2016-02-21 00:35:43 +0200

    operation list

--- client/app/domain/operation/list/operationList.controller.js
+++ client/app/domain/operation/list/operationList.controller.js
@@ -2,8 +2,39 @@
 
 (function () {
 
-  class OperationListController {
-    constructor() {}
+  function OperationListController (
+    $scope, Operation, Agent, CounterAgent, Currency, SettingsService
+    //, ErrorsService
+  ){
+    var vm = this;
+
+    function setAgent(oa, na) {
+
+      var agent = na || oa;
+
+      if (!agent) {
+        return;
+      }
+
+      var f = {owner: agent.id};
+
+      vm.currentAgent = agent;
+
+      Operation.bindAll({}, $scope, 'vm.operations');
+      Operation.findAll(f,{bypassCache:true}).then(function (os) {
+        CounterAgent.findAll();
+      });
+
+    }
+
+    $scope.$on('current-agent', setAgent);
+
+    setAgent (SettingsService.getCurrentAgent());
+
+    angular.extend(vm, {
+
+    });
+
   }
 
   angular.module('authApiApp')
