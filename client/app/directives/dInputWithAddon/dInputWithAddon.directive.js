a8295f5e8f9d5edd97c7626e884b734551fd9b58
Albert <kovalevskij.albert@gmail.com>
2016-02-29 14:52:53 +0200

    directive for dInputWithAddon

---
+++ client/app/directives/dInputWithAddon/dInputWithAddon.directive.js
@@ -0,0 +1,34 @@
+'use strict';
+
+(function (ng) {
+
+  ng.module('authApiApp.directives')
+    .directive('dInputWithAddon', function () {
+      return {
+
+        restrict: 'AC',
+        templateUrl: 'app/directives/dInputWithAddon/dInputWithAddon.html',
+        replace: true,
+        scope: {
+          dSelectModel: '=',
+          dInputModel: '=',
+          dLabelProp: '@',
+          dValueProp: '@',
+          dSelectOptions: '='
+        },
+
+        controller: function ($scope) {
+
+          var vm = this;
+          vm.setActiveItem = function (item) {
+            $scope.dSelectModel = item;
+          };
+
+        },
+        controllerAs: 'vm'
+
+      };
+    })
+  ;
+
+})(angular);
