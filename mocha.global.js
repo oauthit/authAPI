cdf5104c85adad12036b5486893dc3ac12e6eb78
Albert <kovalevskij.albert@gmail.com>
2016-03-02 09:07:13 +0200

    remove mongoose

--- mocha.global.js
+++ mocha.global.js
@@ -1,8 +1,6 @@
 import app from './';
-import mongoose from 'mongoose';
 
 after(function(done) {
   app.angularFullstack.on('close', () => done());
-  mongoose.connection.close();
   app.angularFullstack.close();
 });
