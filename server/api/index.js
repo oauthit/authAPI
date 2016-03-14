7af78fe8d6c5086819408f664112d1a7da3c97e1
Albert <kovalevskij.albert@gmail.com>
2016-03-14 17:49:27 +0200

    apply checkUser

--- server/api/index.js
+++ server/api/index.js
@@ -1,12 +1,13 @@
 'use strict';
 
 import {isAuthenticated} from '../auth/auth.service';
+import checkUser from '../middleware/checkAuthenticated.middleware';
 
 var router = require('express').Router();
 
 router.use('/token', isAuthenticated(), require('./token'));
 router.use('/account', isAuthenticated(), require('./account'));
 router.use('/agent', isAuthenticated(), require('./agent'));
-router.use('/facebook/friend', isAuthenticated(), require('./social/facebook/friend'));
+router.use('/facebook/friend', isAuthenticated(), checkUser(), require('./social/facebook/friend'));
 router.use('/facebook', isAuthenticated(), require('./social/facebook'));
 export default router;
