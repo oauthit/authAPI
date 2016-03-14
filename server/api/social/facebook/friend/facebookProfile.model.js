3795aea192fc8f09c34ff7a3241ed28b99c5c74a
Albert <kovalevskij.albert@gmail.com>
2016-03-14 16:59:04 +0200

    Facebook profile model

--- server/api/social/facebook/friend/facebookProfile.model.js
+++ server/api/social/facebook/friend/facebookProfile.model.js
@@ -1,6 +1,7 @@
 'use strict';
 import redisWrapper from '../../../../config/redis';
 import config from '../../../../config/environment';
+import FB from 'fb';
 
 function saveProfile(profileId, data) {
   return redisWrapper.hsetAsync(config.redisTables.FACEBOOK_PROFILE, profileId, data);
@@ -10,7 +11,39 @@ function getProfile(profileId) {
   return redisWrapper.hgetAsync(config.redisTables.FACEBOOK_PROFILE, profileId);
 }
 
+function getFacebookProfileFromFbApi(id, providerToken, profileId) {
+  return new Promise(function (resolve, reject) {
+    try {
+      var parsed = JSON.parse(providerToken);
+      FB.api(id, {access_token: parsed.accessToken}, function (res) {
+        if (!res || res.error) {
+          getProfile(profileId).then(function (reply) {
+            try {
+              return resolve(JSON.parse(reply));
+            } catch (err) {
+              return reject(err);
+            }
+          });
+        }
+
+        save(res.id, JSON.stringify(res));
+        resolve(res);
+      });
+    } catch (err) {
+      return reject(err);
+    }
+  });
+}
+
+function onReject(response, status) {
+  return function (err) {
+    debug('onReject:', err);
+    response.status(status || 500).end(err);
+  }
+}
+
 export default {
   save: saveProfile,
-  get: getProfile
+  getFromRedis: getProfile,
+  getFromFbApi: getFacebookProfileFromFbApi
 }
