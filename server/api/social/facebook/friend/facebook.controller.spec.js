da82aec8f08150e5a58de9f25ba560e0c278ac84
Albert <kovalevskij.albert@gmail.com>
2016-03-14 12:58:53 +0200

    tests for Facebook ctrl

--- server/api/social/facebook/friend/facebook.controller.spec.js
+++ server/api/social/facebook/friend/facebook.controller.spec.js
@@ -1,13 +1,79 @@
 'use strict';
+import request from 'supertest';
+import app from '../../../../app';
+import sinon from 'sinon';
+import ProviderToken from '../../../providerToken/providerToken.model';
+import FB from 'fb';
+import FacebookFriend from './facebookFriend.model';
+import FacebookProfile from './facebookProfile.model';
+
+//TODO setup test token
+const token = 'fb1d3fce-70a0-4b13-99a9-351b49b42607';
 
 describe('facebook controller', function () {
 
-  it('should get facebook friends list when GET /api/facebook/friend', function () {
-    //TODO write test
+  describe('/api/facebook/friend', function () {
+    var ProviderTokenSpy, FbApiSpy, FacebookFriendSpy;
+    beforeEach(function () {
+      ProviderTokenSpy = sinon.spy(ProviderToken, 'findByProfileId');
+      FbApiSpy = sinon.spy(FB, 'api');
+      FacebookFriendSpy = sinon.spy(FacebookFriend, 'saveAll');
+    });
+
+    afterEach(function () {
+      ProviderTokenSpy.restore();
+      FbApiSpy.restore();
+      FacebookFriendSpy.restore();
+    });
+
+    it('should get facebook friends list when GET /api/facebook/friend', function (done) {
+
+      request(app)
+        .get('/api/facebook/friend')
+        .set('authorization', token)
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .end(function (err, res) {
+          if (err) done(err);
+          res.body.should.be.instanceOf(Array);
+          expect(ProviderTokenSpy.callCount).to.be.eq(1);
+          expect(FbApiSpy.callCount).to.be.eq(1);
+          expect(FacebookFriendSpy.callCount).to.be.eq(1, 'FacebookFriend.saveAll() should be called once');
+          done();
+        });
+
+    });
   });
 
-  it('should get facebook friend by profile id when GET /api/facebook/friend/:id', function () {
-    //TODO write test
+  describe('/api/facebook/friend/:id', function () {
+
+    var ProviderTokenSpy, FacebookProfileSpy;
+    beforeEach(function () {
+      ProviderTokenSpy = sinon.spy(ProviderToken, 'findByProfileId');
+      FacebookProfileSpy = sinon.spy(FacebookProfile, 'get');
+    });
+
+    afterEach(function () {
+      ProviderTokenSpy.restore();
+      FacebookProfileSpy.restore();
+    });
+
+    it('should get facebook friend by profile id when GET /api/facebook/friend/:id', function (done) {
+
+      request(app)
+        .get('/api/facebook/friend/1')
+        .set('authorization', token)
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .end(function (err, res) {
+          if (err) done(err);
+          res.body.should.be.instanceOf(Object);
+          expect(ProviderTokenSpy.callCount).to.be.eq(1, 'ProviderToken.findByProfileId(id) should be called once');
+          expect(FacebookProfileSpy.callCount).to.be.eq(1, 'FacebookProfile.get(id) should be called once');
+          done();
+        });
+
+    });
   });
 
 });
