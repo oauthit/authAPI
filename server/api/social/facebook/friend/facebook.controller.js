9964aa4975544a9da25336ec666c8740a3fd43fd
Albert <kovalevskij.albert@gmail.com>
2016-03-14 17:50:27 +0200

    clean up ctrl

--- server/api/social/facebook/friend/facebook.controller.js
+++ server/api/social/facebook/friend/facebook.controller.js
@@ -1,12 +1,3 @@
-/**
- * Using Rails-like standard naming convention for endpoints.
- * GET     /api/agent              ->  index
- * POST    /api/agent              ->  create
- * GET     /api/agent/:id          ->  show
- * PUT     /api/agent/:id          ->  update
- * DELETE  /api/agent/:id          ->  destroy
- */
-
 'use strict';
 
 import config from '../../../../config/environment';
@@ -36,7 +27,7 @@ function getFromFbApi(id, response, providerToken, profileId) {
   FacebookProfile.getFromFbApi(id, providerToken, profileId).then(function (res) {
     return response.status(200).json(res);
   }, function (err) {
-    return onReject(response, 500) (err);
+    return onReject(response, 500)(err);
   });
 }
 
@@ -49,61 +40,40 @@ Object.assign(ctrl, {
     }
 
     let profileId = req.user.profileId;
-    let provider = req.user.provider;
 
-    ProviderToken.findByProfileId(provider, profileId).then(function (res) {
+    try {
+      var parsed = JSON.parse(req.providerToken);
+      FB.api('me/friends', {access_token: parsed.accessToken, limit: 10}, function (res) {
+        if (!res || res.error) {
+          //if fb not returning data get it from redis
+          FacebookFriend.getAll(profileId).then(function (reply) {
+            if (!reply) {
+              return response.status(404).end('No data');
+            }
 
-      if (!res) {
-        return onReject(response, 401)('No redis data');
-      }
+            try {
+              return response.status(200).json(JSON.parse(reply));
+            } catch (err) {
+              return onReject(response)(err);
+            }
 
-      try {
-        var parsed = JSON.parse(res);
-        FB.api('me/friends', {access_token: parsed.accessToken, limit: 10}, function (res) {
-          if (!res || res.error) {
-            //if fb not returning data get it from redis
-            FacebookFriend.getAll(profileId).then(function (reply) {
-              if (!reply) {
-                return response.status(404).end('No data');
-              }
-
-              try {
-                return response.status(200).json(JSON.parse(reply));
-              } catch (err) {
-                return onReject(response)(err);
-              }
-
-            });
-          }
-
-          FacebookFriend.saveAll(profileId, JSON.stringify(res.data));
-          _.each(res.data, function (profile) {
-            FacebookProfile.save(profile.id, JSON.stringify(profile));
           });
+        }
 
-          return response.status(200).json(res.data);
+        FacebookFriend.saveAll(profileId, JSON.stringify(res.data));
+        _.each(res.data, function (profile) {
+          FacebookProfile.save(profile.id, JSON.stringify(profile));
         });
-      } catch (err) {
-        return onReject(response)(err);
-      }
 
-    }, onReject(response));
+        return response.status(200).json(res.data);
+      });
+    } catch (err) {
+      return onReject(response)(err);
+    }
 
   },
 
-
-  /**
-   * Refreshes user token
-   *
-   * @param req
-   * @param response
-   * @returns {*}
-   */
   refreshToken: (req, response) => {
-
-    if (!req.user) {
-      return response.end('Unauthorized!');
-    }
 
     refreshToken(req.user.provider, req.user.profileId).then(function () {
       response.end();
@@ -114,36 +84,27 @@ Object.assign(ctrl, {
   },
 
   getById: (req, response) => {
-    if (!req.user) {
-      return response.end('Unauthorized!');
-    }
 
     let profileId = req.user.profileId;
     let id = req.params.id;
+    let providerToken = req.providerToken;
 
     /**
-     * Looks for provider token in redis, returns promise, if resolved, returns provider token
+     * Looks for facebook profile in redis, returns promise, if resolved, returns facebook profile
      */
-    ProviderToken.findByProfileId(id, profileId).then(function (providerToken) {
-
-      /**
-       * Looks for facebook profile in redis, returns promise, if resolved, returns facebook profile
-       */
-      FacebookProfile.getFromRedis(id).then(function (reply) {
-        if (!reply) {
-          return getFromFbApi(id, response, providerToken, profileId);
-        }
-
-        try {
-          return response.status(200).json(JSON.parse(reply));
-        } catch (err) {
-          return onReject(response)(err);
-        }
-      }, function () {
+    FacebookProfile.getFromRedis(id).then(function (reply) {
+      if (!reply) {
         return getFromFbApi(id, response, providerToken, profileId);
-      });
+      }
 
-    }, onReject(response));
+      try {
+        return response.status(200).json(JSON.parse(reply));
+      } catch (err) {
+        return onReject(response)(err);
+      }
+    }, function () {
+      return getFromFbApi(id, response, providerToken, profileId);
+    });
 
   }
 
