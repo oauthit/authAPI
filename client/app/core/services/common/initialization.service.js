f78076c721dae89248c944d05f4aefc75beda7ad
Albert <kovalevskij.albert@gmail.com>
2016-03-19 20:07:37 +0200

    init service, test commented

--- client/app/core/services/common/initialization.service.js
+++ client/app/core/services/common/initialization.service.js
@@ -3,12 +3,17 @@
 (function () {
 
   angular.module('authApiApp.core.services')
-    .factory('InitService', function (Currency, CounterAgent, Agent) {
+    .factory('InitService', function (Currency, CounterAgent, Account, Agent) {
 
       function init () {
         Currency.findAll();
         Agent.findAll();
         //CounterAgent.findAll();
+        Account.find('me').then(function(res) {
+          Account.loadRelations(res)
+            .then(function (res) {console.log(res);})
+            .catch(function (err) {console.log(err);});
+        });
       }
 
       return {
