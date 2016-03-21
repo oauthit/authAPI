0aa43c67ed4fa155202178d1c34670eaedca8581
Albert <kovalevskij.albert@gmail.com>
2016-03-21 11:03:18 +0200

    fix for Facebook friend

--- server/middleware/setProviderToken.middleware.js
+++ server/middleware/setProviderToken.middleware.js
@@ -2,6 +2,9 @@
 import ProviderToken from '../api/providerToken/providerToken.model';
 import providerAccount from '../api/providerAccount/providerAccount.model';
 let ProviderAccount = providerAccount();
+var debug = require('debug')('authAPI:setProviderToken.middleware');
+import account from '../api/account/account.model';
+let Account = account();
 
 function onReject(response, status) {
   return function (err) {
@@ -13,17 +16,19 @@ export default function () {
   return function (req, res, next) {
     let id = req.user.id;
 
-    ProviderAccount.findOne({accountId: id}).then(function (data) {
-      req.user.profileId = data.profileId;
-      ProviderToken.findByProfileId(data.provider, data.profileId).then(function (providerToken) {
-        if (!res) {
-          return onReject(res, 401)('Unauthorized!');
-        }
+    Account.findById(id).then(function (account) {
+      ProviderAccount.findById(account.currentProviderAccountId).then(function (data) {
+        req.user.profileId = data.profileId;
+        ProviderToken.findByProfileId(data.provider, data.profileId).then(function (providerToken) {
+          if (!res) {
+            return onReject(res, 401)('Unauthorized!');
+          }
 
-        req.providerToken = providerToken;
-        next();
-      }, onReject(res, 500));
+          req.providerToken = providerToken;
+          next();
+        }, onReject(res, 500));
 
+      });
     });
   }
 }
