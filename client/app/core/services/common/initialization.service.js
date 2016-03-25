574bcb579757e72aeac9a7a808b97648ca9883d2
Albert <kovalevskij.albert@gmail.com>
2016-03-25 12:15:25 +0200

    remove account initialisation

--- client/app/core/services/common/initialization.service.js
+++ client/app/core/services/common/initialization.service.js
@@ -3,17 +3,11 @@
 (function () {
 
   angular.module('authApiApp.core.services')
-    .factory('InitService', function (Currency, CounterAgent, Account, Agent) {
+    .factory('InitService', function (Currency, Agent) {
 
       function init () {
         Currency.findAll();
         Agent.findAll();
-        //CounterAgent.findAll();
-        Account.find('me').then(function(res) {
-          Account.loadRelations(res)
-            .then(function (res) {console.log(res);})
-            .catch(function (err) {console.log(err);});
-        });
       }
 
       return {
