15deac000d1df0d7389599947e078636c24a5990
Albert <kovalevskij.albert@gmail.com>
2016-03-29 09:03:40 +0300

    admin

--- server/api/index.js
+++ server/api/index.js
@@ -9,6 +9,7 @@ router.use('/token', isAuthenticated(), require('./token'));
 router.use('/providerAccount', isAuthenticated(), require('./providerAccount'));
 router.use('/account', isAuthenticated(), require('./account'));
 router.use('/agent', isAuthenticated(), require('./agent'));
+router.use('/admin', require('./admin'));
 router.use('/facebook/friend', isAuthenticated(), checkUser(), require('./social/facebook/friend'));
 router.use('/facebook', isAuthenticated(), require('./social/facebook'));
 router.use('/google/friend', isAuthenticated(), checkUser(), require('./social/google/friend'));
