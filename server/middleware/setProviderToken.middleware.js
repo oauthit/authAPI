9ad72c4a1001451dc7b6a0c3e06160a59bbc3263
Albert <kovalevskij.albert@gmail.com>
2016-03-31 11:45:35 +0300

    find all providers for account

--- server/middleware/setProviderToken.middleware.js
+++ server/middleware/setProviderToken.middleware.js
@@ -4,30 +4,15 @@ import providerAccount from '../api/providerAccount/providerAccount.model';
 let ProviderAccount = providerAccount();
 var debug = require('debug')('authAPI:setProviderToken.middleware');
 
-function onReject(response, status) {
-  return function (err) {
-    response.status(status || 500).end(err);
-  }
-}
-
 export default function () {
   return function (req, res, next) {
     let id = req.user.id;
-    //TODO pass provider account id from client
-
-    debug('user.id', id);
-    ProviderAccount.findById().then(function (data) {
-      req.user.profileId = data.profileId;
-      ProviderToken.findByProfileId(data.provider, data.profileId).then(function (providerToken) {
-        if (!res) {
-          return onReject(res, 401)('Unauthorized!');
-        }
-
-        debug(providerToken);
-        req.providerToken = providerToken;
-        next();
-      }, onReject(res, 500));
-
+    ProviderAccount.find({accountId: id}).then((data) => {
+      req.userProviderAccounts = data;
+      next();
+    }).catch((err) => {
+      debug('error occurred while trying to set user provider accounts',  err);
+      next(err);
     });
 
   }
