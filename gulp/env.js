98adb8ffb3661c705fb0b3ada7ef36522d56da3d
Albert <kovalevskij.albert@gmail.com>
2016-04-18 13:09:18 +0300

    refactor gulp tasks

---
+++ gulp/env.js
@@ -0,0 +1,30 @@
+import gulp from 'gulp';
+import gulpLoadPlugins from 'gulp-load-plugins';
+
+let plugins = gulpLoadPlugins();
+
+/********************
+ * Env
+ ********************/
+
+gulp.task('env:all', () => {
+  let localConfig;
+  try {
+    localConfig = require(`./${serverPath}/config/local.env`);
+  } catch (e) {
+    localConfig = {};
+  }
+  plugins.env({
+    vars: localConfig
+  });
+});
+gulp.task('env:test', () => {
+  plugins.env({
+    vars: {NODE_ENV: 'test'}
+  });
+});
+gulp.task('env:prod', () => {
+  plugins.env({
+    vars: {NODE_ENV: 'production'}
+  });
+});
