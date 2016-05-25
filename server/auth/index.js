c7ad4f3985a90c2738a9ad98246300041b6c8a51
Albert <kovalevskij.albert@gmail.com>
2016-05-25 15:45:08 +0300

    register routes and strategies from providerApp table

--- server/auth/index.js
+++ server/auth/index.js
@@ -4,17 +4,39 @@ import express from 'express';
 import passport from 'passport';
 import config from '../config/environment';
 import providerAccount from '../models/providerAccount/providerAccount.model.js';
-
-// Passport Configuration
-//require('./email/passport').setup();
-require('./facebook/passport').setup(providerAccount(), config);
-require('./google/passport').setup(providerAccount(), config);
+import stapi from '../models/abstract.model';
+const providerApp = stapi('/aa/providerApp');
+import winston from 'winston';
 
 var router = express.Router();
 
-router.use('/email', require('./email'));
-router.use('/pha', require('./pha'));
-router.use('/facebook', require('./facebook'));
-router.use('/google', require('./google'));
+// Passport Configuration
+
+providerApp().find()
+  .then((providerApps) => {
+    providerApps.forEach((providerApp) => {
+      switch (providerApp.provider) {
+        case 'facebook': {
+          require('./facebook/passport').setup(providerAccount(), providerApp);
+          router.use('/'+providerApp.id, require('./facebook')(providerApp.id));
+          break;
+        }
+        case 'google': {
+          require('./google/passport').setup(providerAccount(), providerApp);
+          router.use('/'+providerApp.id, require('./google')(providerApp.id))
+        }
+      }
+    });
+  })
+  .catch((err) => {
+    winston.log('error', err);
+  })
+;
+
+//
+//router.use('/email', require('./email'));
+//router.use('/pha', require('./pha'));
+//router.use('/facebook', require('./facebook'));
+//router.use('/google', require('./google'));
 
 export default router;
