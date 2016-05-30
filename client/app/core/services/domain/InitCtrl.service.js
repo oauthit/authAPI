b695403a45119ff71e1099e2ad5e87c19dcbf54a
Albert <kovalevskij.albert@gmail.com>
2016-05-30 17:52:54 +0300

    initCtrl service

---
+++ client/app/core/services/domain/InitCtrl.service.js
@@ -0,0 +1,23 @@
+(function () {
+
+  'use strict';
+
+  angular.module('authApiApp.core.services')
+    .factory('InitCtrlService', function (saNgTable) {
+
+      function setup(ctrl) {
+        return angular.extend(ctrl, {
+          setupNgTable: function setupNgTable(model) {
+            return saNgTable.setup(ctrl, model);
+          }
+        });
+      }
+
+      return {
+        setup
+      }
+
+    })
+  ;
+
+})();
