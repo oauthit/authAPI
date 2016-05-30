63f634871cb77feae529a3bb16d3237dab30af74
Albert <kovalevskij.albert@gmail.com>
2016-05-30 17:52:34 +0300

    providerApp ctrl

---
+++ server/api/providerApp/providerApp.controller.js
@@ -0,0 +1,8 @@
+'use strict';
+
+import ProviderApp from './../../models/js-data/providerApp.model';
+import abstractCtrl from '../abstract/abstract.stapi.controller';
+
+let ctrl = abstractCtrl(ProviderApp);
+
+export default ctrl;
