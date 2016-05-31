ceb87b4e23c1a8c467a3fedd22c356837927ca20
Albert <kovalevskij.albert@gmail.com>
2016-05-31 14:22:21 +0300

    org

---
+++ server/api/org/org.controller.js
@@ -0,0 +1,8 @@
+'use strict';
+
+import Org from './../../models/org.model';
+import abstractCtrl from '../abstract/abstract.stapi.controller';
+
+let ctrl = abstractCtrl(Org);
+
+export default ctrl;
