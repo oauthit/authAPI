198d8b837ac72f3bde554e767e82e8d63dda7f89
Albert <kovalevskij.albert@gmail.com>
2016-06-02 12:01:12 +0300

    catch errors

--- server/models/js-data/findOrCreate.js
+++ server/models/js-data/findOrCreate.js
@@ -2,13 +2,16 @@
 var debug = require('debug')('authAPI:js-data:findOrCreate');
 
 function findOrCreate(id, body) {
-  return new Promise((fulfil) => {
+  return new Promise((fulfil, reject) => {
 
     if (!id) {
       this.create(body)
         .then((res) => {
           debug('created:', res);
           return fulfil(res);
+        })
+        .catch((err) => {
+          return reject(err);
         })
       ;
     }
