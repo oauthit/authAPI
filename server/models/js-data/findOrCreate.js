6f2f73862642e02073b6e9e89c0d09a5ce5f4452
Albert <kovalevskij.albert@gmail.com>
2016-06-02 10:59:56 +0300

    if not passed then create new record

--- server/models/js-data/findOrCreate.js
+++ server/models/js-data/findOrCreate.js
@@ -3,24 +3,36 @@ var debug = require('debug')('authAPI:js-data:findOrCreate');
 
 function findOrCreate(id, body) {
   return new Promise((fulfil) => {
-    this.find(id)
-      .then(res => {
-        debug('found by id:', res);
-        return fulfil(res);
-      }, () => {
-        this.create(body)
-          .then(res => {
-            debug('created:', res);
-            return fulfil(res);
-          })
-      })
-      .catch(() => {
-        this.create(body)
-          .then(res => {
-            debug('created:', body);
-            return fulfil(res);
-          })
-      })
+
+    if (!id) {
+      this.create(body)
+        .then((res) => {
+          debug('created:', res);
+          return fulfil(res);
+        })
+      ;
+    }
+    else {
+      this.find(id)
+        .then(res => {
+          debug('found by id:', res);
+          return fulfil(res);
+        }, () => {
+          this.create(body)
+            .then(res => {
+              debug('created:', res);
+              return fulfil(res);
+            })
+        })
+        .catch(() => {
+          this.create(body)
+            .then(res => {
+              debug('created:', body);
+              return fulfil(res);
+            })
+        })
+      ;
+    }
   });
 }
 
