9f424358ed896c153f7bebf2077356e702bfeec8
Albert <kovalevskij.albert@gmail.com>
2016-06-07 16:29:18 +0300

    decorator for routeChange handling

--- client/app/router.decorator.js
+++ client/app/router.decorator.js
@@ -1,36 +1,44 @@
 'use strict';
 
-(function() {
+(function () {
 
   angular.module('authApiApp')
-    .run(function($rootScope, $state, Auth, sabErrorsService) {
+    .run(function ($rootScope, $state, Auth, sabErrorsService) {
       // Redirect to login if route requires auth and the user is not logged in, or doesn't have required role
-      $rootScope.$on('$stateChangeStart', function(event, next) {
+      $rootScope.$on('$stateChangeStart', (event, next) => {
         sabErrorsService.clear();
         if (!next.authenticate) {
           return;
         }
 
         if (typeof next.authenticate === 'string') {
-          saAuth.hasRole(next.authenticate, _.noop).then(function (has) {
-            if (has) {
-              return;
-            }
+          Auth.hasRole(next.authenticate, _.noop)
+            .then((has) => {
+              if (has) {
+                return;
+              }
 
-            event.preventDefault();
-            return Auth.isLoggedIn(_.noop).then(function (is) {
-              $state.go(is ? 'main' : 'login');
+              event.preventDefault();
+              return Auth.isLoggedIn(_.noop).then((is) => {
+                $state.go(is ? 'auth.main' : 'auth.login');
+              });
+            })
+            .catch((err) => {
+              console.log(err);
             });
-          });
         } else {
-          Auth.isLoggedIn(_.noop).then(function (is) {
-            if (is) {
-              return;
-            }
+          Auth.isLoggedIn(_.noop)
+            .then((is) => {
+              if (is) {
+                return;
+              }
 
-            event.preventDefault();
-            $state.go('main');
-          });
+              event.preventDefault();
+              $state.go('main');
+            })
+            .catch((err) => {
+              console.log(err);
+            });
         }
       });
     });
