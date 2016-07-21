30951d918382893b43f86f4095cf5ebbd3e65b77
Alexander Levin <alevin.ru@gmail.com>
2016-07-21 11:20:56 +0300

    environment/shared env.STAPI

--- server/config/environment/shared.js
+++ server/config/environment/shared.js
@@ -4,5 +4,5 @@ exports = module.exports = {
   // List of user roles
   userRoles: ['guest', 'user', 'admin'],
   jsDataBasePath: 'http://localhost:9000/api/opr/',
-  STAPI: 'http://localhost:9000/api/'
+  STAPI: (process.env.STAPI || 'http://localhost:9000/api') + '/'
 };
