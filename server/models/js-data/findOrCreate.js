7444102ed8878d073f2d5bba9824cad795724e4e
Alexander Levin <alevin.ru@gmail.com>
2016-07-23 15:35:46 +0300

    reject on findOrCreate.catch

--- server/models/js-data/findOrCreate.js
+++ server/models/js-data/findOrCreate.js
@@ -1,45 +1,35 @@
 "use strict";
-var debug = require('debug')('authAPI:js-data:findOrCreate');
+var debug = require('debug')('authAPI:models:js-data:findOrCreate');
 
 function findOrCreate(id, body) {
   return new Promise((fulfil, reject) => {
 
     if (!id && body) {
+
       this.create(body)
         .then((res) => {
-
           debug('created:', res);
           return fulfil(res);
-        })
-        .catch((err) => {
-          return reject(err);
-        })
-      ;
-    }
-    else {
+        }, reject);
+
+    } else {
+
       this.find(id)
         .then(res => {
           debug('found by id:', res);
           return fulfil(res);
-        }, () => {
-          this.create(body)
-            .then(res => {
-              debug('created:', res);
-              return fulfil(res);
-            });
         })
         .catch(() => {
           this.create(body)
             .then(res => {
               debug('created:', body);
               return fulfil(res);
-            });
-        })
-      ;
+            }, reject);
+        });
+
     }
+
   });
 }
 
-export default {
-  findOrCreate
-};
+export {findOrCreate};
