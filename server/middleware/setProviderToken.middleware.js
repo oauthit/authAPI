c62146ef18c814a16654827ec468180168dc4e25
Alexander Levin <alevin.ru@gmail.com>
2016-07-23 20:18:48 +0300

    simplify setProviderToken.middleware

--- server/middleware/setProviderToken.middleware.js
+++ server/middleware/setProviderToken.middleware.js
@@ -1,19 +1,18 @@
 'use strict';
-import ProviderToken from '../models/providerToken.model.js';
+
 import providerAccount from '../models/providerAccount/providerAccount.model.js';
 let ProviderAccount = providerAccount();
-var debug = require('debug')('authAPI:setProviderToken.middleware');
 
 export default function () {
   return function (req, res, next) {
+
     let id = req.user.id;
+
     ProviderAccount.find({accountId: id}).then((data) => {
       req.userProviderAccounts = data;
       next();
-    }).catch((err) => {
-      debug('error occurred while trying to set user provider accounts',  err);
-      next(err);
-    });
+    })
+      .catch(next);
 
   };
 }
