e2f5bf0cf76f155ef0ad7cc223fb610649ff8456
Grigoriev Maxim <grigoriev.maxim@gmail.com>
2016-07-28 21:44:55 +0300

    move DEBUG colors to local.env.js

--- server/index.js
+++ server/index.js
@@ -3,9 +3,6 @@
 // Set default node environment to development
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-process.env.DEBUG_FD = 1;
-process.env.DEBUG_COLORS= 'true';
-
 if (env === 'development' || env === 'test') {
   // Register the Babel require hook
   require('babel-core/register');
