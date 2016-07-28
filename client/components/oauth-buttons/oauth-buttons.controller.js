e53a7726975a97998f99d5c756f1c79f4805a776
Grigoriev Maxim <grigoriev.maxim@gmail.com>
2016-07-28 22:16:45 +0300

    oauthButtons refactor

--- client/components/oauth-buttons/oauth-buttons.controller.js
+++ client/components/oauth-buttons/oauth-buttons.controller.js
@@ -5,28 +5,6 @@ angular.module('authApiApp')
 
     var OauthButtons = this;
 
-    OauthButtons.buttons = [{
-      aClass: 'btn-facebook',
-      iClass: 'fa fa-facebook',
-      oauthPath: 'facebook/debtee',
-      title: 'Facebook'
-    }, {
-      aClass: 'btn-google',
-      iClass: 'fa fa-google-plus',
-      oauthPath: 'google/debtee',
-      title: 'Google+'
-    }, {
-      aClass: 'btn-twitter',
-      iClass: ['fa', 'fa-twitter'],
-      oauthPath: 'twitter',
-      title: 'Twitter'
-    }, {
-      aClass: 'btn-openid',
-      iClass: ['glyphicon', 'glyphicon-phone'],
-      oauthPath: 'sms/sms',
-      title: 'mobile number'
-    }];
-
     OauthButtons.loginOauth = function(provider) {
       $window.location.href = '/auth/' + provider;
     };
