833d1e2db70a9ae216c452246abc15a928f3ddc5
Albert <kovalevskij.albert@gmail.com>
2016-07-29 13:46:26 +0300

    rm jsDataBasePath

--- server/config/environment/shared.js
+++ server/config/environment/shared.js
@@ -3,7 +3,6 @@
 exports = module.exports = {
   // List of user roles
   userRoles: ['guest', 'user', 'admin'],
-  jsDataBasePath: 'http://localhost:9000/api/opr/',
   STAPI: (process.env.STAPI || 'http://localhost:9000/api') + '/',
   smsAuth: {
     url: process.env.SMSAUTH || 'http://localhost:3000'
