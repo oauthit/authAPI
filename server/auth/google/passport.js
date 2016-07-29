39a5034f7bf989a425565b0328877dbc7cf5ada6
Albert <kovalevskij.albert@gmail.com>
2016-07-29 13:36:53 +0300

    rm comments

--- server/auth/google/passport.js
+++ server/auth/google/passport.js
@@ -1,31 +1,4 @@
 import {Strategy as GoogleStrategy} from 'passport-google-oauth2';
-// var debug = require('debug')('authAPI:google/passport');
-
 import basePassportSetup from '../basePassportSetup';
 
 export default basePassportSetup(GoogleStrategy);
-
-// export function setup(ProviderAccount, providerApp) {
-//   var strategy = new GoogleStrategy({
-//     clientID: providerApp.clientId,
-//     clientSecret: providerApp.clientSecret,
-//     callbackURL: `${process.env.DOMAIN || ''}/auth/${providerApp.provider}/${providerApp.name}/callback`,
-//     passReqToCallback: true
-//   }, (request, accessToken, refreshToken, profile, done) => {
-//
-//     ProviderAccount.getOrCreate({
-//       profileId: profile.id
-//     }, {
-//       profileData: profile,
-//       profileId: profile.id,
-//       name: profile.displayName,
-//       roles: [],
-//       accessToken: accessToken,
-//       refreshToken: refreshToken,
-//       providerAppId: providerApp.id
-//     }).then(passportCb(providerApp.provider, profile, done), done);
-//   });
-//
-//   strategy.name = 'google' + providerApp.code;
-//   return strategy;
-// }
