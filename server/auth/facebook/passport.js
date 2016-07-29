217229e428140bdbd2d957ef8e94c244d46c4c66
Albert <kovalevskij.albert@gmail.com>
2016-07-29 13:35:58 +0300

    rm comments

--- server/auth/facebook/passport.js
+++ server/auth/facebook/passport.js
@@ -5,44 +5,4 @@ var debug = require('debug')('authAPI:facebook/passport');
 // import refresh_token from '../../api/social/facebook/refreshToken.service';
 import basePassportSetup from '../basePassportSetup';
 
-console.error('facebook');
-
-
 export default basePassportSetup(FacebookStrategy);
-
-// export function setup(ProviderAccount, providerApp) {
-//   var strategy = new FacebookStrategy({
-//     clientID: providerApp.clientIdp
-//     clientSecret: providerApp.clientSecret,
-//     callbackURL: `${process.env.DOMAIN || ''}/auth/${providerApp.provider}/${providerApp.name}/callback`,
-//     passReqToCallback: true
-//   }, (req, accessToken, refreshToken, profile, done) => {
-//
-//     function processToken(refToken) {
-//
-//       debug('refreshToken:', refToken);
-//
-//       ProviderAccount.getOrCreate({
-//           profileId: profile.id
-//         }, {
-//           profileData: profile,
-//           profileId: profile.id,
-//           name: profile.displayName,
-//           roles: [],
-//           accessToken: accessToken,
-//           refreshToken: refToken && JSON.stringify(refToken) || null,
-//           providerAppId: providerApp.id
-//         })
-//         .then(passportCb(providerApp.provider, profile, done), done);
-//
-//     }
-//
-//     //check if it necessary
-//     // refresh_token('facebook', profile.id).then(processToken, function () {
-//     //   processToken();
-//     // });
-//   });
-//
-//   strategy.name = 'facebook' + providerApp.code;
-//   return strategy;
-// }
