dfcc01cb846f52af6622bd238754eb82c1395e24
Alexander Levin <alevin.ru@gmail.com>
2016-07-31 22:14:21 +0300

    ensureItsMe

--- server/middleware/checkAuthenticated.middleware.js
+++ server/middleware/checkAuthenticated.middleware.js
@@ -1,17 +1,40 @@
 'use strict';
 
+var debug = require('debug')('authAPI:middleware:checkAuthenticated');
+
 function onReject(response, status) {
   return function (err) {
     response.status(status || 500).end(err);
   };
 }
 
-export default function () {
+function ensureAuthenticated () {
   return function (req, res, next) {
     if (!req.user) {
-      return onReject(res, 401) ('Unauthorized!');
+      return res.sendStatus(401);
     } else {
       next();
     }
   };
 }
+
+
+function ensureItsMe (req, res, next) {
+
+  if (!req.params) {
+    req.params = {};
+  }
+
+  req.params.id = req.user && req.user.id;
+
+  if (!req.params.id) {
+    res.sendStatus(403);
+  }
+
+  debug ('ensureItsMe', req.params.id);
+
+  next();
+
+}
+
+export {ensureAuthenticated, ensureItsMe};
