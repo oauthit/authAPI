b44ef5ee8a125e68061c2d0afb04ec32ca3cedb2
Alexander Levin <alevin.ru@gmail.com>
2016-07-31 22:15:20 +0300

    account.controller use stapi

--- server/api/account/account.controller.js
+++ server/api/account/account.controller.js
@@ -2,53 +2,5 @@
 
 import stapiAccount from './../../models/account.model.js';
 import {stapiBaseController} from 'sistemium-node';
-import Account from '../../models/js-data/account.model';
-var debug = require('debug')('authAPI:api:account:controller');
 
-let abstractController = stapiBaseController;
-let ctrl = abstractController(stapiAccount);
-
-function setReq(req) {
-  if (!req.params) {
-    req.params = {};
-  }
-  req.params.id = req.user && req.user.id || 0;
-  debug('setReq user:', req.user);
-  return req;
-}
-
-ctrl.index = function (req, res) {
-
-  Account.findAll().then(data => {
-    return res.json(data);
-  }).catch(err => {
-    console.error(err);
-  });
-
-};
-
-ctrl.showMe = function (req, res) {
-  req = setReq(req);
-  req.params.id = req.user.tokenInfo.id;
-  ctrl.show(req, res);
-};
-
-ctrl.updateMe = function (req, res) {
-  ctrl.update(setReq(req), res);
-};
-
-ctrl.create = function (req, res) {
-  Account.create(req.body)
-    .then(account => {
-      debug('create Account:', account);
-      // return status 204 created
-      return res.sendStatus(204).json(account);
-    })
-    .catch(err => {
-      console.error(err);
-      return res.sendStatus(500);
-    })
-  ;
-};
-
-export default ctrl;
+export default stapiBaseController(stapiAccount);
