5cdcd88e1995b3835607ec7d341e50d28dd98008
Albert <kovalevskij.albert@gmail.com>
2016-08-02 12:09:02 +0300

    add state for org create

--- client/app/domain/organizations/org.router.js
+++ client/app/domain/organizations/org.router.js
@@ -8,6 +8,12 @@
           templateUrl: 'app/domain/organizations/org.html',
           controller: 'OrgController',
           controllerAs: 'vm'
+        })
+        .state('auth.orgCreate', {
+          url: '/org/create',
+          templateUrl: 'app/domain/organizations/create/orgCreate.html',
+          controller: 'OrgCreateController',
+          controllerAs: 'vm'
         });
     });
 
