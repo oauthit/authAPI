8ed762c03bf4cbd2f44ac9b71bce09181dc5b071
Albert <kovalevskij.albert@gmail.com>
2016-08-02 14:38:41 +0300

    rm unused code

--- server/auth/passportCallback.js
+++ server/auth/passportCallback.js
@@ -1,10 +1,6 @@
 'use strict';
 
-import Token from '../models/js-data/token.model';
 import ProviderToken from '../models/providerToken.model.js';
-import account from '../models/account.model.js';
-
-var Account = account();
 var debug = require('debug')('authAPI:auth:passportCallback');
 
 export default (provider, profile, done) => {
@@ -12,26 +8,8 @@ export default (provider, profile, done) => {
 
     ProviderToken.createToken(provider, profile.id, data.accessToken, data.refreshToken)
       .then(function () {
-
         debug('token created:', data);
-
-        if (data.accountId) {
-          Account.findById(data.accountId)
-            .then((account) => {
-
-              debug('account data:', account);
-              Token.create({tokenInfo: account})
-                .then(token => {
-                  done(null, data, token);
-                }, done)
-              ;
-            }, done)
-            .catch(done);
-        } else {
-          debug(data);
-          done(null, data);
-        }
-
+        done(null, data);
       })
       .catch(done);
 
