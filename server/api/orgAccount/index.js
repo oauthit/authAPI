0b4418618c0cef6e4c3cac30aea17d19c10b6063
Albert <kovalevskij.albert@gmail.com>
2016-08-03 18:19:46 +0300

    orgAccount put post

--- server/api/orgAccount/index.js
+++ server/api/orgAccount/index.js
@@ -1,12 +1,32 @@
 'use strict';
 
+import {setAccount} from '../../middleware/authHelpers.middleware';
+import stapiOrgAccount from './../../models/orgAccount.model.js';
+
 var express = require('express');
 var controller = require('./orgAccount.controller.js');
 
 var router = express.Router();
+
 router.get('/', controller.index);
 router.get('/:id', controller.show);
 
-router.post('/', controller.create);
+function checkIfAlreadyMember (req, res, next) {
+
+  stapiOrgAccount(req)
+    .find({
+      orgId: req.body.orgId,
+      accountId: req.query.accountId
+    })
+    .then((orgAccounts)=>{
+      if (orgAccounts.length){
+        req.params.id = orgAccounts[0].id;
+      }
+      next();
+    });
+
+}
+router.post('/', setAccount, checkIfAlreadyMember, controller.create);
+router.put('/:id', setAccount, checkIfAlreadyMember, controller.create);
 
 module.exports = router;
