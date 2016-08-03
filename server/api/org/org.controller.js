8a857e0b67e3a4e9db7ab35fdd36f8e8594dbb18
Albert <kovalevskij.albert@gmail.com>
2016-08-03 18:18:49 +0300

    org.controller with onStapiSuccess

--- server/api/org/org.controller.js
+++ server/api/org/org.controller.js
@@ -15,26 +15,28 @@ function show(req, res) {
   ctrl.show(req, res);
 }
 
-function create(req, res) {
-  ctrl.create(req, res, org => {
+function create(req, res, next) {
 
-      return Promise.all([
-        orgAccountRole(req).save({
-          orgId: org.id,
-          accountId: req.user.id,
-          // TODO: get public roles at bootstrap
-          roleId: '08af0df4-588d-11e6-8000-e188647b398f'
-        }), orgAccount(req).save({
-          orgId: org.id,
-          accountId: req.user.id,
-          name: req.user.name
-        })
-      ]).then(() => {
-        return org;
-      });
+  req.onStapiSuccess = org => {
 
-    }
-  );
+    return Promise.all([
+      orgAccountRole(req).save({
+        orgId: org.id,
+        accountId: req.user.id,
+        // TODO: get public roles at bootstrap
+        roleId: '08af0df4-588d-11e6-8000-e188647b398f'
+      }), orgAccount(req).save({
+        orgId: org.id,
+        accountId: req.user.id,
+        name: req.user.name
+      })
+    ]).then(() => {
+      return org;
+    });
+
+  };
+
+  ctrl.create(req, res, next);
 }
 
 export default {index, show, create};
