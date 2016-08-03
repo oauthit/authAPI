c6313a19d0826463917ceacc5317542fec72ed97
Alexander Levin <alevin.ru@gmail.com>
2016-08-03 20:36:21 +0300

    joinOrg with formly

--- client/app/domain/organizationInfo/orgInfo.controller.js
+++ client/app/domain/organizationInfo/orgInfo.controller.js
@@ -2,12 +2,14 @@
   'use strict';
 
   angular.module('authApiApp')
-    .controller('OrgInfoController', function (schema, $state, $stateParams, $scope, Auth) {
+    .controller('OrgInfoController', function (schema, $state, $stateParams, $scope, Auth, saFormlyConfigService) {
 
       var vm = this;
 
       var Org = schema.model('Org');
       var OrgAccount = schema.model('OrgAccount');
+
+      var joinFields = saFormlyConfigService.getConfigFieldsByKey('OrgAccount.join');
 
       var stateFilter = {
         id: $state.params.orgId
@@ -20,38 +22,43 @@
       Org.findAll(stateFilter);
       Org.bindOne(stateFilter.id, $scope, 'vm.org');
 
+
+
       Auth.getCurrentUser(user => {
 
-        var orgAccount = {
+        var orgAccountFilter = {
           orgId: stateFilter.id,
           accountId: user.id
         };
 
-        OrgAccount.findAll(orgAccount, {bypassCache: true})
+        OrgAccount.findAll(orgAccountFilter, {bypassCache: true})
           .then(oa=>{
             vm.orgAccount = oa.length && oa[0] ||
-              OrgAccount.createInstance(angular.extend(orgAccount,{
+              OrgAccount.createInstance(angular.extend(orgAccountFilter,{
                 name: user.name
               }));
           });
 
-
-
       });
 
       angular.extend(vm, {
+
+        joinFields: joinFields,
+
         join: function () {
           OrgAccount.create(vm.orgAccount)
             .then(()=>{
               Org.find(stateFilter.id, {bypassCache: true});
             });
         },
+
         leave: function () {
           OrgAccount.destroy(vm.orgAccount)
             .then(()=>{
               Org.find(stateFilter.id, {bypassCache: true});
             });
         }
+
       });
 
     })
