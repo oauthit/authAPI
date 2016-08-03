9680f1068194458b082bbe9e3c2aec2463a7bf56
Albert <kovalevskij.albert@gmail.com>
2016-08-03 11:06:59 +0300

    create orgAccount on org create

--- server/api/org/org.controller.js
+++ server/api/org/org.controller.js
@@ -2,6 +2,7 @@
 
 import stapiOrg from './../../models/org.model.js';
 import orgAccountRole from './../../models/orgAccountRole.model.js';
+import orgAccount from './../../models/orgAccount.model.js';
 import {stapiBaseController} from 'sistemium-node';
 
 let ctrl = stapiBaseController(stapiOrg);
@@ -19,15 +20,24 @@ function find(req, res) {
 }
 
 function create(req, res) {
-  ctrl.create(req, res, org =>
+  ctrl.create(req, res, org => {
 
-    orgAccountRole(req).save({
-      orgId: org.id,
-      accountId: req.user.id,
-      // TODO: get public roles at bootstrap
-      roleId: '08af0df4-588d-11e6-8000-e188647b398f'
-    })
+      return Promise.all([
+        orgAccountRole(req).save({
+          orgId: org.id,
+          accountId: req.user.id,
+          // TODO: get public roles at bootstrap
+          roleId: '08af0df4-588d-11e6-8000-e188647b398f'
+        }), orgAccount(req).save({
+          orgId: org.id,
+          accountId: req.user.id,
+          name: req.user.name
+        })
+      ]).then(() => {
+        return org;
+      });
 
+    }
   );
 }
 
