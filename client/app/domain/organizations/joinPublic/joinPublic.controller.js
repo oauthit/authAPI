1dd211e93f7e714e8246376315c5c8ccf8c968a1
Albert <kovalevskij.albert@gmail.com>
2016-08-03 13:47:47 +0300

    joinPublic with isMember

--- client/app/domain/organizations/joinPublic/joinPublic.controller.js
+++ client/app/domain/organizations/joinPublic/joinPublic.controller.js
@@ -2,14 +2,16 @@
   'use strict';
 
   angular.module('authApiApp')
-    .controller('OrgJoinPublicController', function (InitCtrlService, schema) {
+    .controller('OrgJoinPublicController', function ($scope, InitCtrlService, schema, Auth) {
 
       let vm = InitCtrlService.setup(this);
       var Org = schema.model('Org');
+      var stateFilter = {
+        isPublic: true
+      };
 
-      Org.findAll({isPublic: true}).then(res => {
-        vm.publicOrgs = res;
-      });
+      Org.findAllWithRelations(stateFilter)('OrgAccount');
+      Org.bindAll(stateFilter, $scope, 'vm.orgs');
 
       angular.extend(vm, {
         ngTable: {
