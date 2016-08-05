a3b0bc88a44f489f07184741cd4594cf58ba29c6
Albert <kovalevskij.albert@gmail.com>
2016-08-05 18:13:39 +0300

    authMiddleware isAuthenticated(), setAccount, setOrgApp

--- server/api/index.js
+++ server/api/index.js
@@ -1,19 +1,21 @@
 'use strict';
 
 import {isAuthenticated} from '../auth/auth.service';
+import {setAccount, setOrgApp} from '../middleware/authHelpers.middleware';
 
 var router = require('express').Router();
-var authMiddleware = isAuthenticated();
+var authMiddleware = [isAuthenticated(), setAccount, setOrgApp];
 
 router.use('/token', authMiddleware, require('./token'));
 router.use('/providerAccount', /*authMiddleware,*/ require('./providerAccount'));
 router.use('/providerApp', /*authMiddleware*/ require('./providerApp'));
-router.use('/org', authMiddleware, require('./org'));
-router.use('/app', authMiddleware, require('./app'));
-router.use('/orgApp', authMiddleware, require('./orgApp'));
-router.use('/orgAccount', authMiddleware, require('./orgAccount'));
+
+router.use('/org', ...authMiddleware, require('./org'));
+router.use('/app', ...authMiddleware, require('./app'));
+router.use('/orgApp', ...authMiddleware, require('./orgApp'));
+router.use('/orgAccount', ...authMiddleware, require('./orgAccount'));
 router.use('/orgProviderApp', /*authMiddleware*/ require('./orgProviderApp'));
-router.use('/account', authMiddleware, require('./account'));
+router.use('/account', ...authMiddleware, require('./account'));
 router.use('/admin', require('./admin'));
 
 export default router;
