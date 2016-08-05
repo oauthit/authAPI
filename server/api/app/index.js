546ca06580785e73fa8a95cc16bbbc5720b162e6
Albert <kovalevskij.albert@gmail.com>
2016-08-05 18:14:40 +0300

    middleware to get app by req.query.appId

--- server/api/app/index.js
+++ server/api/app/index.js
@@ -2,8 +2,12 @@
 
 var express = require('express');
 import controller from './app.controller.js';
+import {stripIdFromName} from '../../middleware/authHelpers.middleware';
 
 var router = express.Router();
+
+router.use(stripIdFromName('app'));
+
 router.get('/', controller.index);
 router.get('/:id', controller.show);
 
