2d011cd1a8cbe6a3e5bb6d7704c8eac50e3f2c1d
Albert <kovalevskij.albert@gmail.com>
2016-08-05 14:58:17 +0300

    orgAccountRole controller

---
+++ server/api/orgAccountRole/orgAccountRole.controller.js
@@ -0,0 +1,8 @@
+'use strict';
+
+import stapiOrgAccountRole from './../../models/orgAccountRole.model.js';
+import {stapiBaseController} from 'sistemium-node';
+
+let ctrl = stapiBaseController(stapiOrgAccountRole);
+
+export default ctrl;
