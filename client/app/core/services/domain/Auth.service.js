bc9fcc3953587432a2699304e604ed69f1175a23
Alexander Levin <alevin.ru@gmail.com>
2016-08-08 13:01:06 +0300

    Auth.loginOauth

--- client/app/core/services/domain/Auth.service.js
+++ client/app/core/services/domain/Auth.service.js
@@ -3,7 +3,7 @@
 (function () {
 
   angular.module('authApiApp.core.services')
-    .factory('Auth', function (saAuth,schema) {
+    .factory('Auth', function (saAuth, schema, saToken, $window) {
 
       var config = {
         authUrl: '',
@@ -11,10 +11,22 @@
       };
 
       let Auth = saAuth(config);
+
       angular.extend(Auth, {
         getCurrentRoles: function () {
           //TODO api route for currentRoles
         },
+        loginOauth: function(providerApp) {
+          if (!providerApp) {
+            return console.error('Auth.loginOauth providerApp undefined');
+          }
+          var href = `/auth/${providerApp.provider}/${providerApp.name}`;
+          var token = saToken.get();
+          if (token) {
+            href += `?access_token=${token}`;
+          }
+          $window.location.href = href;
+        }
       });
 
       return Auth;
