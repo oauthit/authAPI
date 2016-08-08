6a5d17e2f0e37cc88307da4749566808b3b84703
Albert <kovalevskij.albert@gmail.com>
2016-08-08 16:53:26 +0300

    setCurrentOrg

---
+++ client/app/core/services/domain/settings.service.js
@@ -0,0 +1,44 @@
+'use strict';
+
+(function () {
+
+  angular.module('authApiApp.core.services').factory('SettingsService', function ($timeout, schema, $rootScope, localStorageService) {
+
+    var currentOrg;
+    var Org = schema.model('Org');
+
+    function setCurrentOrg(org) {
+      if (!org) {
+        return;
+      }
+      localStorageService.set('current-org-id', org.id);
+      $rootScope.$broadcast('current-org', currentOrg = org);
+    }
+
+    function getCurrentOrg() {
+      return currentOrg;
+    }
+
+    function setCurrentOrgOnChange() {
+      var orgs = Org.getAll();
+
+      if (!currentOrg || !_.find(orgs, { id: currentOrg.id })) {
+        var id = localStorageService.get('current-org-id');
+        setCurrentOrg(id && Org.get(id) || _.head(orgs));
+      }
+    }
+
+    Org.on('DS.afterInject', function () {
+      $timeout(setCurrentOrgOnChange());
+    });
+
+    Org.on('DS.afterDestroy', function () {
+      $timeout(setCurrentOrgOnChange);
+    });
+
+    return {
+      setCurrentOrg: setCurrentOrg,
+      getCurrentOrg: getCurrentOrg
+    };
+  });
+})();
--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -1,16 +1,24 @@
 'use strict';
 
-function NavbarController($state,
+function NavbarController($state, $rootScope, $scope,
+                          schema,
                           Modal,
-                          Auth) {
+                          Auth,
+                          SettingsService) {
 
   var vm = this;
+  var Org = schema.model('Org');
 
   var logout = function () {
     Modal.confirm.confirm(function () {
       $state.go('auth.logout');
     })();
   };
+
+  function setCurrentOrg(org) {
+    SettingsService.setCurrentOrg(org);
+    vm.currentOrg = org;
+  }
 
   angular.extend(vm, {
     menu: [{
@@ -23,9 +31,17 @@ function NavbarController($state,
     isLoggedIn: Auth.isLoggedIn,
     isAdmin: Auth.isAdmin,
     getCurrentUser: Auth.getCurrentUser,
-    logout: logout
+    logout: logout,
+    setCurrentOrg: setCurrentOrg
   });
 
+  $rootScope.$on('logged-in', function () {
+    Org.bindAll({}, $scope, 'nav.orgs');
+  });
+
+  $rootScope.$on('current-org', function (ev, org) {
+    vm.currentOrg = org;
+  });
 
 }
 
