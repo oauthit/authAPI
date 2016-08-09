b5d9a781ff81beda7da91096c58dac24cc28575c
Albert <kovalevskij.albert@gmail.com>
2016-08-09 15:41:50 +0300

    route for orgRole

--- server/api/index.js
+++ server/api/index.js
@@ -10,6 +10,7 @@ router.use('/token', authMiddleware, require('./token'));
 router.use('/providerAccount', /*authMiddleware,*/ require('./providerAccount'));
 router.use('/providerApp', /*authMiddleware*/ require('./providerApp'));
 router.use('/role', authMiddleware, require('./role'));
+router.use('/orgRole', authMiddleware, require('./orgRole'));
 
 router.use('/org', ...authMiddleware, require('./org'));
 router.use('/app', ...authMiddleware, require('./app'));
