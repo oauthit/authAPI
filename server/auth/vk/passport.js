05ce5de253b7c6869a54539ff705b421b0e2b1b6
Alexander Levin <alevin.ru@gmail.com>
2016-08-10 15:22:29 +0300

    fix VkontakteStrategy _apiVersion

--- server/auth/vk/passport.js
+++ server/auth/vk/passport.js
@@ -3,4 +3,19 @@
 import {Strategy as VkontakteStrategy} from 'passport-vkontakte';
 import basePassportSetup from '../basePassportSetup';
 
-export default basePassportSetup(VkontakteStrategy);
+export default basePassportSetup(VkontakteStrategy,{
+  _apiVersion: null,
+  userProfile: function(accessToken, done) {
+    VkontakteStrategy.prototype.userProfile.call(this, accessToken,(err, profile)=>{
+      if (err) {
+        done(err);
+      } else {
+        profile.id = profile.id || profile.username;
+        if (/^id[\d]+/.test(profile.id)) {
+          profile.id = profile.id.replace(/^id/,'')
+        }
+        done(null, profile);
+      }
+    });
+  }
+});
