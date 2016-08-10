5e1d4736ca5b4a5a83bcf2b2553aa183b090878a
Albert <kovalevskij.albert@gmail.com>
2016-08-10 15:29:03 +0300

    cleanup ctrl

--- client/app/domain/organizations/organizationInfo/setOrgRoles.controller.js
+++ client/app/domain/organizations/organizationInfo/setOrgRoles.controller.js
@@ -7,22 +7,13 @@
       var vm = this;
       vm.roles = [];
 
-      var OrgRole = schema.model('OrgRole');
       var Role = schema.model('Role');
-
-      OrgRole.findAll();
-      OrgRole.bindAll({}, $scope, 'vm.orgRoles');
 
       Role.findAll({}, {bypassCache: true});
       Role.bindAll({}, $scope, 'vm.roles');
 
       $scope.refreshRoles = function (searchFor) {
-        console.log(searchFor);
-        return Role.findAll({"searchFor:": searchFor, "searchFields:": ['name']}, {bypassCache: true})
-          .then((roles) => {
-            console.log(vm.roles);
-            console.log(roles);
-          });
+        return Role.findAll({'searchFor:': searchFor, 'searchFields:': ['name']}, {bypassCache: true});
       };
 
       angular.extend(vm, {
