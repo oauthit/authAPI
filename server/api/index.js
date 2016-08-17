4a4b95265238f501f44bbceb93dd22dfc1056d75
Albert <kovalevskij.albert@gmail.com>
2016-08-17 19:30:02 +0300

    orgAccountRole

--- server/api/index.js
+++ server/api/index.js
@@ -10,13 +10,14 @@ router.use('/token', authMiddleware, require('./token'));
 router.use('/providerAccount', /*authMiddleware,*/ require('./providerAccount'));
 router.use('/providerApp', /*authMiddleware*/ require('./providerApp'));
 router.use('/role', authMiddleware, require('./role'));
-router.use('/orgRole', authMiddleware, require('./orgRole'));
 
 router.use('/org', ...authMiddleware, require('./org'));
-router.use('/app', ...authMiddleware, require('./app'));
-router.use('/orgApp', ...authMiddleware, require('./orgApp'));
+router.use('/orgRole', authMiddleware, require('./orgRole'));
 router.use('/orgAccount', ...authMiddleware, require('./orgAccount'));
+router.use('/orgAccountRole', ...authMiddleware, require('./orgAccountRole'));
+router.use('/orgApp', ...authMiddleware, require('./orgApp'));
 router.use('/orgProviderApp', /*authMiddleware*/ require('./orgProviderApp'));
+router.use('/app', ...authMiddleware, require('./app'));
 router.use('/account', ...authMiddleware, require('./account'));
 router.use('/admin', require('./admin'));
 
