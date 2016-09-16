75d8cbe9429ebb653a7f5b59d74e82a58786a2db
Albert <kovalevskij.albert@gmail.com>
2016-09-16 13:20:14 +0300

    route for post

--- server/api/providerApp/index.js
+++ server/api/providerApp/index.js
@@ -6,5 +6,6 @@ var controller = require('./providerApp.controller.js');
 var router = express.Router();
 router.get('/', controller.index);
 router.get('/:id', controller.show);
+router.post('/', controller.create);
 
 module.exports = router;
