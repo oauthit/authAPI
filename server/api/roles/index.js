3e09f809b4eb74783f19954711c588ecf64c6945
Alexander Levin <alevin.ru@gmail.com>
2016-10-25 15:54:24 +0300

    api/roles

---
+++ server/api/roles/index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+import _ from 'lodash';
+import express from 'express';
+import {stapiBaseController, baseStapiModel} from 'sistemium-node';
+
+var router = express.Router();
+var model = baseStapiModel('/aa/orgAccountRoleHelper');
+
+var ctrl = stapiBaseController(req => {
+  return {
+    find: () => model(req).find()
+      .then(response => {
+        var grouped = _.groupBy(response, 'code');
+        var res = {};
+        _.each(grouped, (values, code) => {
+          var value = _.map(values, value => value.value || true);
+          res[code] = value.length === 1 ? value[0] : value;
+        });
+        return res;
+      })
+  };
+});
+
+router.get('/', ctrl.index);
+
+module.exports = router;
