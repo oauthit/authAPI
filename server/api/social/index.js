ef7d1597092438fe9cdfa7d940c820b10fcffad3
Alexander Levin <alevin.ru@gmail.com>
2017-09-08 11:48:25 +0300

    disable vk for now

--- server/api/social/index.js
+++ server/api/social/index.js
@@ -4,7 +4,7 @@ var express = require('express');
 var router = express.Router();
 
 router.use('/google', require('./google'));
-router.use('/vk', require('./vk'));
+//router.use('/vk', require('./vk'));
 router.use('/facebook', require('./facebook'));
 router.use('/socialAccount', require('./socialAccountSTAPI'));
 
