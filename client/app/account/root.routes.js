2dbc599c6d20e9787e92bc50ce362abeaa1cf671
Alexander Levin <alevin.ru@gmail.com>
2017-09-13 11:19:21 +0300

    formatting

--- client/app/account/root.routes.js
+++ client/app/account/root.routes.js
@@ -1,32 +1,52 @@
 'use strict';
 
-angular.module('authApiApp')
-  .config(function($stateProvider) {
+(function () {
+
+  angular.module('authApiApp')
+
+    .config(rootRoutes)
+
+    .run($rootScope => {
+      $rootScope.$on('$stateChangeStart', logoutReferrerDecorator);
+    });
+
+  function logoutReferrerDecorator(event, next, nextParams, current) {
+    if (next.name === 'logout' && current && current.name && !current.authenticate) {
+      next.referrer = current.name;
+    }
+  }
+
+  function rootRoutes($stateProvider) {
+
     $stateProvider
+
       .state('auth.login', {
         url: '/login',
         templateUrl: 'app/account/login/login.html',
         controller: 'LoginController',
         controllerAs: 'vm'
       })
+
       .state('auth.logout', {
         url: '/logout?referrer',
         referrer: 'auth.main',
         template: '',
-        controller: function($state, Auth) {
-          var referrer = $state.params.referrer ||
-                          $state.current.referrer ||
-                          'auth.main';
+        controller: function ($state, Auth) {
+          let referrer = $state.params.referrer ||
+            $state.current.referrer ||
+            'auth.main';
           Auth.logout();
           $state.go(referrer);
         }
       })
+
       .state('auth.signup', {
         url: '/signup',
         templateUrl: 'app/account/signup/signup.html',
         controller: 'SignupController',
         controllerAs: 'vm'
       })
+
       .state('auth.settings', {
         url: '/settings',
         templateUrl: 'app/account/settings/settings.html',
@@ -34,11 +54,7 @@ angular.module('authApiApp')
         controllerAs: 'vm',
         authenticate: true
       });
-  })
-  .run(function($rootScope) {
-    $rootScope.$on('$stateChangeStart', function(event, next, nextParams, current) {
-      if (next.name === 'logout' && current && current.name && !current.authenticate) {
-        next.referrer = current.name;
-      }
-    });
-  });
+
+  }
+
+})();
